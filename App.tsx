21e5374f933c4bc58227bb22a44d3aa6ac45e0a2
dohee12 <happyddo42@gmail.com>
2025-03-11 12:55:17 +0900

    header 구현 및 디자인

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // Logic Process
+  // 1. onClick (버튼)
+  // 1-a. function 키워드
+  function 이름() {
+
+  }
+  // 1-b. arrow function
+  // let : 재선언 가능
+  // const : 재선언 불가능능
+  const onClick = () => {
+    // 알림창
+    Alert.alert("버튼 클릭됨")
+  }
+
+  // UI Page Rendering
   return (
-    <View style={styles.container}> 
-      <Text>hello word</Text> {/* 택스트 */}
-      <StatusBar style="auto" /> {/* 상단 상태바 */}
-    </View> // <View>, </View>, <Text>, <StatusBar> = Component
+    <SafeAreaView style={styles.container}>
+      <View style={{paddingTop: 20}}> 
+        <View style={styles.header}>
+          <Text>InstaDaelim</Text>
+          <Button onPress={onClick} title='create'/>
+        </View>
+        <Text>hello word</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // 영역에서 차지하는 비율 (1 = 100)
-    backgroundColor: '#fff', // 백그라운드 배경
-    alignItems: 'center', // 왼쪽, 오른쪽, 중간
-    justifyContent: 'center', // 위, 중간, 아래
+    flex: 1,
+    backgroundColor: '#fff',
   },
+  header: {
+    height : 50,
+    backgroundColor: 'lightgray',
+    flexDirection : 'row', // 축의 방향을 변경
+    justifyContent : 'space-between',
+    alignItems : 'center',
+    paddingHorizontal : 10
+  } 
 });
