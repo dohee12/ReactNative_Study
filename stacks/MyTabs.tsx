a227b70e09f2ad4d15ab11a1205eb233591d8386
dohee12 <happyddo42@gmail.com>
2025-03-18 17:24:41 +0900

    하단 탭 메뉴에 Home, MyInfo 배치

---
+++ stacks/MyTabs.tsx
@@ -0,0 +1,15 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MyInfo from "./MyInfo";
+import Home from "../screens/Home";
+
+// Tab용 navigation 컴포넌트 생성
+const Tab = createBottomTabNavigator();
+
+export default () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="MyInfo" component={MyInfo} />
+    </Tab.Navigator>
+  );
+};
