48d33fb8b87632f1937d08f71dd9edf8ac53a043
dohee12 <happyddo42@gmail.com>
2025-04-15 18:46:22 +0900

    feat: UploadPost 페이지 추가

---
+++ screens/UploadPost.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import styled from "styled-components";
+
+const Container = styled(View)``;
+const Title = styled(Text)``;
+
+export default () => {
+  4;
+  return (
+    <Container>
+      <Title>Upload Page......</Title>
+    </Container>
+  );
+};
