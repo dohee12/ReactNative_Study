168d206e718db722b596594a33b3f6b2f66d2de2
dohee12 <happyddo42@gmail.com>
2025-04-15 18:45:28 +0900

    feat: UploadPost 스크린 추가

--- stacks/MainStack.tsx
+++ stacks/MainStack.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import CreatePost from "../screens/CreatePost";
+import UploadPost from "../screens/UploadPost";
 import MyTabs from "./MyTabs";
 
 const Stack = createStackNavigator();
@@ -13,7 +14,9 @@ export default () => {
         component={MyTabs}
         options={{ headerShown: false }}
       />
+      {/* 보이지 않지만 등록되어, 언제든 이동할 수 있는 스크린 */}
       <Stack.Screen name="CreatePost" component={CreatePost} />
+      <Stack.Screen name="UploadPost" component={UploadPost} />
     </Stack.Navigator>
   );
 };
