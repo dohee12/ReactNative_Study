dffb4e23315272ec2aa3ea7cb6cf7f19682e8065
dohee12 <happyddo42@gmail.com>
2025-05-01 21:33:22 +0900

    refactor:  주석 추가

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -2,16 +2,20 @@ import { ActivityIndicator, View } from "react-native";
 import styled from "styled-components";
 import { primaryColor } from "../styles/style";
 
+// 화면 전체를 감싸는 컨테이너 스타일 정의
 const Container = styled(View)`
-  flex: 1;
-  background-color: #fff;
+  flex: 1; /* 화면 전체를 차지 */
+  background-color: #fff; /* 배경색을 흰색으로 설정정 */
   justify-content: center; /* 상,중,하에서 가운데 */
   align-items: center; /**왼,중,오에서 가운데데 */
 `;
 
+// LoadingScreen 컴포넌트 정의
 export default () => {
   return (
+    // 화면 렌더링
     <Container>
+      {/* 로딩 스피너 표시 */}
       <ActivityIndicator size={"large"} color={primaryColor} />
     </Container>
   );
