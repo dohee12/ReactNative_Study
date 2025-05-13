ac89b38103967652f78b0e5c554db54f523f3667
dohee12 <happyddo42@gmail.com>
2025-05-13 15:01:00 +0900

    feat: UploadPost, 데이터 Server 업로드, 우측 버튼 생성 및 Container 스타일 수정

--- screens/UploadPost.tsx
+++ screens/UploadPost.tsx
@@ -2,18 +2,66 @@ import { Image, Text, View } from "react-native";
 import styled from "styled-components";
 import { MainStackList } from "../stacks/MainStack";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { TextInput } from "react-native-gesture-handler";
+import { useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import HeaderRightBtn from "../components/HeaderRightBtn";
+import UploadLoadingScreen from "../components/upload-loading-screen";
+import { AntDesign } from "@expo/vector-icons";
 
 // styled-components를 사용하여 스타일링된 컴포넌트 생성
-const Container = styled(View)``; // 화면 전체를 감싸는 컨테이너
+const Container = styled(View)`
+  background-color: white;
+  flex: 1;
+`; // 화면 전체를 감싸는 컨테이너
 const Title = styled(Text)``; // 제목 텍스트 스타일
 
 const PhotoBox = styled(View)`
-  width: 150px;
-  height: 150px;
+  width: 115px;
+  height: 115px;
 `;
 const Photo = styled(Image)`
   width: 100%;
   height: 100%;
+  border-radius: 10px;
+`;
+
+const Icon = styled(Image)`
+  width: 100%;
+  height: 100%;
+`;
+
+const Blind = styled(View)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  opacity: 0.5;
+  border-radius: 10px;
+`;
+
+const IconBox = styled(View)`
+  width: 25%;
+  height: 25%;
+  margin: 5%;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  right: 0px;
+  border-radius: 50%;
+`;
+
+const UploadPost = styled(View)`
+  flex-direction: row;
+  padding: 20px;
+`;
+
+const Caption = styled(View)`
+  flex: 1;
+`;
+
+const CaptionInput = styled(TextInput)`
+  font-size: 20px;
 `;
 
 // 'UploadPost'가 'navigation'을 통해 전달받는 Props의 타입/구조
@@ -26,15 +74,76 @@ export default ({ route: { params } }: Props) => {
   // 전달받은 데이터 중, 임의의 데이터 가져오기
   // 예외처리, 만일 photos 값이 없는 경우(null), 빈배열 반환
   const photos = params.assets ?? [];
+  // State : 내가 작성한 글(Caption)을 저장할 State 생성
+  const [caption, setCaption] = useState<string>("");
+  // State : 로딩화면을 위한 state
+  const [loading, setLoading] = useState<boolean>(false);
+  // Naigation Hook
+  const navi = useNavigation();
+  // 글을 작성할 때 호출되어 caption(state)에 수정한 글 반영
+  const onChangeText = (text: string) => {
+    setCaption(text);
+  };
+
+  // (비동기형) 데이터를 Server에 업로드
+  const onUpload = () => {
+    // 0. 방어코드 : caption을 작성하지 않은 경우 업로드 불가
+    // 1. 필요한 데이터
+    // ㄴ a. caption(작성글) / b. photos(선택한사진들)
+    // 2. 서버에 업로드(with.인터넷 통신.. 속도차이)
+    // 2-a. 정상실행
+    try {
+      // --- 로딩 시작 ---
+      setLoading(true);
+      // 서버 업로드 Process
+      // --- 로딩 완료 ---
+      // setLoading(false);
+      // 로딩 완료 ==> 정상적으로 서버 업로드 완료료
+      // 알림창, 페이지를 홈화면으로 이동
+    } catch (error) {
+      // 2-b. 오류발생
+      // --- 로딩 완료 ---
+      // setLoading(false);
+    }
+  };
+
+  // Header 부분에 Upload 버튼을 만들기 위해 사용
+  useLayoutEffect(() => {
+    // Header 우측에 Upload 버튼 생성
+    navi.setOptions({
+      headerRight: () => <HeaderRightBtn title="Upload" onPress={onUpload} />,
+    });
+  }, []);
 
   return (
     // 화면 렌더링
     <Container>
-      {/* 업로드 페이지 제목 */}
-      <Title>Upload Page......</Title>
-      <PhotoBox>
-        <Photo source={{ uri: photos[0].uri }} />
-      </PhotoBox>
+      <UploadPost>
+        <PhotoBox>
+          <Photo source={{ uri: photos[0].uri }} />
+          <Blind />
+          {/* 선택한 사진이 2장 이상인 경우에만 표현 */}
+          {photos.length > 1 && (
+            <IconBox>
+              {/* <Icon
+            source={require("../assets/icons/9104345_copy_duplicate_paste_clipboard_data_icon.png")}
+          /> */}
+              <AntDesign name="laptop" size={30} color={"white"} />
+            </IconBox>
+          )}
+        </PhotoBox>
+        <Caption>
+          <CaptionInput
+            multiline={true}
+            placeholder="글을 작성해주세요..."
+            placeholderTextColor={"#a8a8a8"}
+            onChangeText={(text) => onChangeText(text)}
+            value={caption}
+          />
+        </Caption>
+      </UploadPost>
+      {/* 업로드 Process가 실행되는 경우에만 나타남 */}
+      {loading && <UploadLoadingScreen />}
     </Container>
   );
 };
