d807e954d6a5c288ca8b46d424efc9e1f662bfb7
dohee12 <happyddo42@gmail.com>
2025-05-26 11:00:14 +0900

    refactor: LoginScreen 경로 오류 수정

--- stacks/AuthStack.tsx
+++ stacks/AuthStack.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoadingScreen from "../screens/LoadingScreen";
+import LoginScreen from "../screens/Login/login-screen";
 
 // [Before Login] 로그인 관련 페이지 등록
 const Stack = createNativeStackNavigator<AuthStackList>();
