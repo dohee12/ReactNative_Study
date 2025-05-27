0beb0ba0f4e80b7a616a9387c4e7b8da06a66136
dohee12 <happyddo42@gmail.com>
2025-05-27 19:26:13 +0900

    feat: Initialize Auth ReactNative 호환 및 Firebase DB, Storage 정보 추가

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,8 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +20,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth 로그인 인증, 유저 정보
-export const auth = initializeAuth(app);
+// Initialize Auth 로그인 인증, 유저 정보 + ReactNative 호환
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+// Initialize Firebase DB에 저장 정보
+export const db = getFirestore(app);
+
+// Initialize Storage에 대한 정보
+export const storage = getStorage(app);
