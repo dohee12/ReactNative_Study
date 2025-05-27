9d824636b67d56d089c43fa4d8f50b38887e3441
dohee12 <happyddo42@gmail.com>
2025-05-27 19:38:54 +0900

    feat: 회원가입 페이지 버튼 구현 및 Error 메시지 초기화, 화면 키패드 추가

--- screens/Login/login-screen.tsx
+++ screens/Login/login-screen.tsx
@@ -7,11 +7,17 @@ import {
   View,
   TextInput,
   Alert,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import styled, { css } from "styled-components";
 import { auth } from "../../firebaseConfig";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNaviProps } from "../../stacks/AuthStack";
 
 const ImgContainer = styled(ImageBackground)`
   background-color: black;
@@ -36,9 +42,6 @@ const InputField = styled(View)`
   gap: 10px;
 `;
 const InputCSS = css`
-  border-radius: 5px;
-  background-color: #e9e9e9;
-  font-size: 16px;
   padding: 10px 15px;
 `;
 const UserEmail = styled(TextInput)`
@@ -47,7 +50,6 @@ const UserEmail = styled(TextInput)`
 const UserPassword = styled(TextInput)`
   ${InputCSS};
 `;
-
 const LoginBtn = styled(TouchableOpacity)`
   background-color: dodgerblue;
   align-items: center;
@@ -74,14 +76,24 @@ const CreateAccountText = styled(Text)`
   font-weight: 600;
   text-decoration: underline;
 `;
+const ErrorBox = styled(View)`
+  justify-content: center;
+  align-items: center;
+`;
+const ErrorMessage = styled(Text)`
+  color: red;
+`;
 
 const logoSource = require("../../assets/resources/instaDaelim_title.png");
 const bgSource = require("../../assets/resources/instaDaelim_background.jpg");
 const LoginScreen = () => {
   // State Input (Email, PassWord)
   const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState<String>("");
-  const [password, setPassword] = useState<String>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [error, setError] = useState<string | null>(null);
+  const navi = useNavigation<AuthNaviProps>();
 
   // email, password 입력 시, state 할당/저장
   const onChangeText = (text: string, type: "email" | "password") => {
@@ -95,12 +107,18 @@ const LoginScreen = () => {
     }
   };
 
+  // 회원가입 버튼을 누르면, 회원가입 페이지로 이동
+  const goToCreateAccount = () => {
+    navi.navigate("CreateAccount");
+  };
+
   // 서버에 Email, Password 전달하여 로그인 인증 진행
   const onUpload = async () => {
     // [방어코드] : email, password 입력 안한 경우에 경고
 
-    // 1. 로딩 시작...
+    // 1. 로딩 시작... & Error 메시지 초기화
     setLoading(true);
+    setError(null);
 
     try {
       // 2. 서버에 Email, Password 전달
@@ -110,12 +128,17 @@ const LoginScreen = () => {
         password
       );
       // 3-1. 로그인 성공 => Home으로 이동
-      Alert.alert("Success", credential.user.email);
+
+      Alert.alert("Login Success");
     } catch (error) {
       // 3-2. 로그인 실패 => 실패문구 표시
       // firebase 관련된 로그인 에러만 표시
       if (error instanceof FirebaseError) {
-        Alert.alert("Error", error.message);
+        console.error("Error", error.code);
+        // 직접 작성한 Firebase 에러코드 타입의 값으로 'key'타입으로 형변환
+        const errorCode = error.code as keyof FirebaseErrorCodeType;
+        // 형변환된 에러코드를 Error state에 할당한다다
+        setError(FirebaseErrorCode[errorCode]);
       }
     } finally {
       // 로딩 종료
@@ -124,41 +147,73 @@ const LoginScreen = () => {
   };
 
   return (
-    <ImgContainer source={bgSource} resizeMode="cover">
-      <LoginBox>
-        {/* 앱 로고 */}
-        <Logo source={logoSource} />
-        <Title>Welcome! 🤗{"\n"}This is instagram for daelim</Title>
-        {/* 유저 Email & PW 입력공간 */}
-        <InputField>
-          <UserEmail
-            value={email}
-            keyboardType="email-address"
-            returnKeyType="next"
-            onChangeText={(text) => {
-              onChangeText(text, "email");
-            }}
-            placeholder="Email *"
-          />
-          <UserPassword
-            secureTextEntry={true}
-            value={password}
-            returnKeyType="done"
-            onChangeText={(text) => {
-              onChangeText(text, "password");
-            }}
-            placeholder="Password *"
-          />
-        </InputField>
-        {/* 로그인 버튼  */}
-        <LoginBtn onPress={loading ? undefined : onUpload}>
-          <LoginBtnTitle>{loading ? "Loading..." : "Log in"}</LoginBtnTitle>
-        </LoginBtn>
-        <CreateAccount>
-          <CreateAccountTitle>Already have an account?</CreateAccountTitle>
-          <CreateAccountText>Create Account</CreateAccountText>
-        </CreateAccount>
-      </LoginBox>
-    </ImgContainer>
+    // 키패드가 화면(입력하는 영역)을 가리지 않도록 화면을 올려서 피한다
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      {/* 키패드가 나타난 경우, 입력창 이외에 화면을 터치했을 때 키패드를 다시 가린다 */}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImgContainer source={bgSource} resizeMode="cover">
+          <LoginBox>
+            {/* 앱 로고 */}
+            <Logo source={logoSource} />
+            <Title>Welcome! 🤗{"\n"}This is instagram for daelim</Title>
+            {/* 유저 Email & PW 입력공간 */}
+            <InputField>
+              <UserEmail
+                value={email}
+                keyboardType="email-address"
+                returnKeyType="next"
+                onChangeText={(text) => {
+                  onChangeText(text, "email");
+                }}
+                placeholder="Email *"
+              />
+              <UserPassword
+                secureTextEntry={true}
+                value={password}
+                returnKeyType="done"
+                onChangeText={(text) => {
+                  onChangeText(text, "password");
+                }}
+                placeholder="Password *"
+              />
+            </InputField>
+            {/* 로그인 버튼  */}
+            <LoginBtn onPress={loading ? undefined : onUpload}>
+              <LoginBtnTitle>{loading ? "Loading..." : "Log in"}</LoginBtnTitle>
+            </LoginBtn>
+            {/* 에러 메시지 출력 */}
+            {!error && (
+              <ErrorBox>
+                <ErrorMessage>{error}</ErrorMessage>
+              </ErrorBox>
+            )}
+            {/* 회원가입 버튼  */}
+            <CreateAccount onPress={goToCreateAccount}>
+              <CreateAccountTitle>Already have an account?</CreateAccountTitle>
+              <CreateAccountText>Create Account</CreateAccountText>
+            </CreateAccount>
+          </LoginBox>
+        </ImgContainer>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
+};
+
+export default LoginScreen;
+
+// Type
+type FirebaseErrorCodeType = {
+  "auth/invalid-email": string;
+  "auth/invalid-credential": string;
+  "auth/missing-password": string;
+};
+
+// Firebase Error Code...
+const FirebaseErrorCode: FirebaseErrorCodeType = {
+  "auth/invalid-email": "invalid email form",
+  "auth/invalid-credential": "잘못된 계정 정보",
+  "auth/missing-password": "비밀번호를 입력하지 않음음",
 };
