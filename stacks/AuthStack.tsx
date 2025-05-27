3e4674e9af348115f658c46463fc3a7f4ad7a4f4
dohee12 <happyddo42@gmail.com>
2025-05-27 19:36:24 +0900

    feat: Create Account 페이지 등록

--- stacks/AuthStack.tsx
+++ stacks/AuthStack.tsx
@@ -1,18 +1,27 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import CreateAccountScreen from "../screens/Login/create-account-screen";
 import LoginScreen from "../screens/Login/login-screen";
 
 // [Before Login] 로그인 관련 페이지 등록
 const Stack = createNativeStackNavigator<AuthStackList>();
 
 // key(screenName) : value(params)
-type AuthStackList = {
+export type AuthStackList = {
   Login: undefined;
+  CreateAccount: undefined;
 };
+
+// navigation type 용
+export type AuthNaviProps = NativeStackNavigationProp<AuthStackList>;
 
 const AuthStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
     </Stack.Navigator>
   );
 };
