b6034a954fb56996d6b68e131e9c2fd828d92d12
dohee12 <happyddo42@gmail.com>
2025-05-27 19:35:06 +0900

    feat: 로딩 페이지 구현

---
+++ screens/Login/loading-screen.tsx
@@ -0,0 +1,11 @@
+import { ActivityIndicator, View } from "react-native";
+
+const LoadingScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <ActivityIndicator size={"large"} color={"dodgerblue"} />
+    </View>
+  );
+};
+
+export default LoadingScreen;
