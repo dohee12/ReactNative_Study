6b37155e6eb097d67e422f6d391c83b38b38e46f
dohee12 <happyddo42@gmail.com>
2025-06-01 12:11:24 +0900

    refactor: loading 페이지 수정

--- screens/Login/loading-screen.tsx
+++ screens/Login/loading-screen.tsx
@@ -2,7 +2,14 @@ import { ActivityIndicator, View } from "react-native";
 
 const LoadingScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
       <ActivityIndicator size={"large"} color={"dodgerblue"} />
     </View>
   );
