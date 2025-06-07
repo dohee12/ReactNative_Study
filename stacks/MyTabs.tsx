2ba24305b55da72b81f9286fd119d1a723348a0d
dohee12 <happyddo42@gmail.com>
2025-06-07 17:47:46 +0900

    feat: Tab.Screen options 추가

--- stacks/MyTabs.tsx
+++ stacks/MyTabs.tsx
@@ -1,14 +1,20 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import MyInfo from "../screens/MyInfo";
 import Home from "../screens/Home";
+import MyInfo from "../screens/MyInfo";
 
-// Tab용 navigation 컴포넌트 생성
+// Bottom Tab 용 navigation 컴포넌트 생성
 const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
       <Tab.Screen name="MyInfo" component={MyInfo} />
     </Tab.Navigator>
   );
