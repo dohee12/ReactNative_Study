2836387af25a8054cce7e46dfe60db3a03fc5509
dohee12 <happyddo42@gmail.com>
2025-06-07 17:49:02 +0900

    docs: AuthStack 주석 수정

--- stacks/AuthStack.tsx
+++ stacks/AuthStack.tsx
@@ -1,11 +1,12 @@
+// [Before Login] 로그인 관련 페이지 등록
+
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from "@react-navigation/native-stack";
-import CreateAccountScreen from "../screens/Login/create-account-screen";
 import LoginScreen from "../screens/Login/login-screen";
+import CreateAccountScreen from "../screens/Login/create-account-screen";
 
-// [Before Login] 로그인 관련 페이지 등록
 const Stack = createNativeStackNavigator<AuthStackList>();
 
 // key(screenName) : value(params)
@@ -13,13 +14,17 @@ export type AuthStackList = {
   Login: undefined;
   CreateAccount: undefined;
 };
-
 // navigation type 용
 export type AuthNaviProps = NativeStackNavigationProp<AuthStackList>;
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        // presentation: "modal",
+      }}
+    >
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
     </Stack.Navigator>
